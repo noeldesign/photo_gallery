d47c8b9f3c93d0044b037a59b8b086dc37d8c15f
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-04-29 12:56:44 -0500

    added a root layout component for nesting routes

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import './App.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/Home';
 import MasonryGrid from './components/MasonryGrid';
+import RootLayout from './pages/Root';
 
 const router = createBrowserRouter([
-  {path: '/', element: <HomePage /> },
-  {path: '/Masonry', element: <MasonryGrid /> },
+  {
+    path: '/', 
+    element: <RootLayout />,
+    children: [
+      {path: '/', element: <HomePage /> },
+      {path: '/Masonry', element: <MasonryGrid /> },
+    ]
+  },
 ])
 
 function App() {
   return (
-    <RouterProvider router={router}>
-      <div className="App"></div>
-    </RouterProvider>
+    <RouterProvider router={router} />
   );
 }
 
