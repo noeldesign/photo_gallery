3e10ffb7898c3f24a02bc5c99f9995d2a5a0bb81
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-04-30 16:52:52 -0500

    imported buffer to test api call

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HomePage from "./pages/Home";
 import RootLayout from "./pages/Root";
 import ErrorPage from "./pages/Error";
 import EventsPage from "./pages/Events";
+import { Buffer } from "buffer"
 
 const router = createBrowserRouter([
     {
